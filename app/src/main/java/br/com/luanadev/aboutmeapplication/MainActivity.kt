80d13c048f3006af3df57d7f807a2f5c62047a35
Luana Souza <luana.souza@ifood.com.br>
2021-06-07 14:37:31 -0300

    Add data binding

--- app/src/main/java/br/com/luanadev/aboutmeapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/aboutmeapplication/MainActivity.kt
@@ -12,39 +12,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val myName = MyName("Luana")
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.myName = myName
-
         binding.doneButton.setOnClickListener {
             addNickname(it)
-        }
-        binding.nicnameText.setOnClickListener {
-            updateNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        binding.apply {
+        binding. apply {
             myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
-            binding.nicknameEdit.visibility = View.GONE
-            binding.doneButton.visibility = View.GONE
-            binding.nicnameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
         }
+
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-
-    private fun updateNickname (view: View) {
-        binding.nicknameEdit.visibility = View.VISIBLE
-        binding.doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
-        binding.nicknameEdit.requestFocus()
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(binding.nicknameEdit, 0)
     }
 }
