d210222649c5f20ab0b8a7ef7fc2c714aa361f70
Luana Souza <luana.souza@ifood.com.br>
2021-06-07 14:40:46 -0300

    Add data binding

--- app/src/main/java/br/com/luanadev/aboutmeapplication/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/aboutmeapplication/MainActivity.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val myName: MyName = MyName("Aleks Haecky")
+    private val myName: MyName = MyName("Luana Souza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
